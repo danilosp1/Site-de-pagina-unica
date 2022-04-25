d73ae4c2940902a0f36ce03f39f99a9e5e7b4296
Danilo da Silva Pinto <danilo.sp03@gmail.com>
2022-04-25 13:57:18 -0300

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const hamburger = document.querySelector('.header .nav-bar .hamburger')
 const mobileMenu = document.querySelector('.header .nav-bar ul')
 const header = document.querySelector('.header.container')
+const selectors = document.querySelectorAll('li > a')
+
+selectors.forEach(selected => {
+    selected.addEventListener('click', e => {
+        if(mobileMenu.classList.contains('active')){
+            hamburger.classList.toggle('active')
+            mobileMenu.classList.toggle('active')
+        }
+    })
+})
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active')
     mobileMenu.classList.toggle('active')
 })
+
 
 document.addEventListener('scroll', () => {
     var scrollPosition = window.scrollY;
